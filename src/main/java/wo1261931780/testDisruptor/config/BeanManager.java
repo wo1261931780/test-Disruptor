149732472be55243eb0d7fbae8ca2b0cf723d966
junw <wo1261931780@gmail.com>
2023-10-20 11:27:08 +0800

    完成技术测试

--- src/main/java/wo1261931780/testDisruptor/config/BeanManager.java
+++ src/main/java/wo1261931780/testDisruptor/config/BeanManager.java
@@ -25,6 +25,13 @@ public class BeanManager implements ApplicationContextAware {
 
 	private static ApplicationContext applicationContext = null;
 
+	/**
+	 * 其实 生成者 -> 消费者 模式是很常见的，通过一些消息队列也可以轻松做到上述的效果。
+	 * 不同的地方在于，Disruptor 是在内存中以队列的方式去实现的，而且是无锁的。这也是 Disruptor 高效的地方
+	 *
+	 * @param applicationContext 上下文
+	 * @throws BeansException 异常
+	 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
--- src/main/java/wo1261931780/testDisruptor/config/MQManager.java
+++ src/main/java/wo1261931780/testDisruptor/config/MQManager.java
@@ -24,33 +24,40 @@ import java.util.concurrent.Executors;
 @Configuration
 public class MQManager {
 	/**
-	 * 定义ringbuffer
+	 * 定义ringbuffer环形的缓冲区。
+	 *
+	 * 曾经 RingBuffer 是 Disruptor 中的最主要的对象，
+	 *
+	 * 但从3.0版本开始，其职责被简化为仅仅负责对通过 Disruptor 进行交换的数据（事件）进行存储和更新。
+	 *
+	 * 在一些更高级的应用场景中，Ring Buffer 可以由用户的自定义实现来完全替代。
+	 *
 	 * @return ringbuffer
 	 */
 	@Bean("messageModel")
 	public RingBuffer<MessageModel> messageModelRingBuffer() {
-		//定义用于事件处理的线程池， Disruptor通过java.util.concurrent.ExecutorSerivce提供的线程来触发consumer的事件处理
+		// 定义用于事件处理的线程池， Disruptor通过java.util.concurrent.ExecutorService提供的线程来触发consumer的事件处理
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 
-		//指定事件工厂
+		// 指定事件工厂
 		HelloEventFactory factory = new HelloEventFactory();
 
-		//指定ringbuffer字节大小，必须为2的N次方（能将求模运算转为位运算提高效率），否则将影响效率
+		// 指定ringbuffer字节大小，必须为2的N次方（能将求模运算转为位运算提高效率），否则将影响效率
 		int bufferSize = 1024 * 256;
 
-		//单线程模式，获取额外的性能
+		// 单线程模式，获取额外的性能
 		Disruptor<MessageModel> disruptor = new Disruptor<>(factory, bufferSize, executor,
 				ProducerType.SINGLE, new BlockingWaitStrategy());
 
-		//设置事件业务处理器---消费者
+		// 设置事件业务处理器---消费者
 		disruptor.handleEventsWith(new HelloEventHandler());
 
 		// 启动disruptor线程
 		disruptor.start();
 
-		//获取ringbuffer环，用于接取生产者生产的事件
-		RingBuffer<MessageModel> ringBuffer = disruptor.getRingBuffer();
-
-		return ringBuffer;
+		// 获取ringbuffer环，用于接取生产者生产的事件
+		// RingBuffer<MessageModel> ringBuffer = disruptor.getRingBuffer();
+		// 直接返回结果即可
+		return disruptor.getRingBuffer();
 	}
 }
